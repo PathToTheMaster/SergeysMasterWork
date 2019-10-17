8e20b59f660104f0421f1cb386a065a4592b5498
Pavel Nikoluyk <pashanikoluyk@outlook.com>
2019-10-17 12:56:05 +0300

    first commit

---
+++ PathToTheMaster/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PathToTheMaster.Web.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+            Database.Migrate();
+        }
+    }
+}
